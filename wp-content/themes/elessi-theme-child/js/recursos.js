03c5fbee5334df8fd3144fe25f7a439f7ca61ff3
Henrique Moraes <chmoraes9@gmail.com>
2022-03-04 13:24:24 -0300

    update

--- wp-content/themes/elessi-theme-child/js/recursos.js
+++ wp-content/themes/elessi-theme-child/js/recursos.js
@@ -1,21 +1,21 @@
-    document.getElementById('nasa-input-mobile-search').placeholder = 'Digite aqui o produto ou categoria';
-    var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-shop').innerHTML;
-    document.querySelector('.nasa-bot-icons.nasa-bot-icon-shop').innerHTML = rp.replace('Shop','Loja');
+document.getElementById('nasa-input-mobile-search').placeholder = 'Digite aqui o produto ou categoria';
+var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-shop').innerHTML;
+document.querySelector('.nasa-bot-icons.nasa-bot-icon-shop').innerHTML = rp.replace('Shop','Loja');
 
-    var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-categories').innerHTML;
-    document.querySelector('.nasa-bot-icons.nasa-bot-icon-categories').innerHTML = rp.replace('Categories','Categorias');
+var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-categories').innerHTML;
+document.querySelector('.nasa-bot-icons.nasa-bot-icon-categories').innerHTML = rp.replace('Categories','Categorias');
 
-    var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-search').innerHTML;
-    document.querySelector('.nasa-bot-icons.nasa-bot-icon-search').innerHTML = rp.replace('Search','Pesquisar');
+var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-search').innerHTML;
+document.querySelector('.nasa-bot-icons.nasa-bot-icon-search').innerHTML = rp.replace('Search','Pesquisar');
 
-    var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-wishlist').innerHTML;
-    document.querySelector('.nasa-bot-icons.nasa-bot-icon-wishlist').innerHTML = rp.replace('Wishlist','Favoritos');
-    jQuery('.nasa-tit-wishlist.nasa-sidebar-tit.text-center')[0].innerText = 'Favoritos';
+var rp = document.querySelector('.nasa-bot-icons.nasa-bot-icon-wishlist').innerHTML;
+document.querySelector('.nasa-bot-icons.nasa-bot-icon-wishlist').innerHTML = rp.replace('Wishlist','Favoritos');
+jQuery('.nasa-tit-wishlist.nasa-sidebar-tit.text-center')[0].innerText = 'Favoritos';
 
-    document.querySelector('.nasa-tit-filter-cat').innerText = 'Categorias';
-    document.querySelector('.nasa-tit-viewed.nasa-sidebar-tit.text-center').innerText = 'Visualizações Recentes';
+document.querySelector('.nasa-tit-filter-cat').innerText = 'Categorias';
+document.querySelector('.nasa-tit-viewed.nasa-sidebar-tit.text-center').innerText = 'Visualizações Recentes';
 
-    document.querySelector('.nasa-login-register-ajax.inline-block').href='http://nova.princessanymore.com.br/login-registro'
+document.querySelector('.nasa-login-register-ajax.inline-block').href='http://nova.princessanymore.com.br/login-registro'
 
 try {
     document.querySelector('#main-content .um-field-block div').innerText = 'Enviamos uma mensagem para o seu e-mail. Por favor siga as etapas descritas no e-mail para trocar a sua senha.';
